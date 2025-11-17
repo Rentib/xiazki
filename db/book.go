5ac58ce3a5559d52018719290cd26abe378d693b
Rentib <sbitner420@tutanota.com>
2025-11-17 19:10:38 +0100

    refactor(db): use RunInTx

--- db/book.go
+++ db/book.go
@@ -3,9 +3,9 @@ package db
 import (
 	"context"
 	"fmt"
-	"net/http"
 	"reflect"
 	"time"
+
 	"xiazki/model"
 
 	"github.com/labstack/echo/v4"
@@ -15,29 +15,20 @@ import (
 func InsertBook(db *bun.DB, c echo.Context, book *model.Book) error {
 	ctx := c.Request().Context()
 
-	tx, err := db.BeginTx(ctx, nil)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to start transaction")
-	}
-	defer tx.Rollback()
-
-	if _, err := tx.NewInsert().Model(book).Exec(ctx); err != nil {
-		return err
-	} else if err := insertBookRelation(ctx, tx, book.ID, book.Authors, newBookAuthor); err != nil {
-		return err
-	} else if err := insertBookRelation(ctx, tx, book.ID, book.Tags, newBookTag); err != nil {
-		return err
-	} else if err := insertBookRelation(ctx, tx, book.ID, book.Translators, newBookTranslator); err != nil {
-		return err
-	} else if err := insertBookRelation(ctx, tx, book.ID, book.Narrators, newBookNarrator); err != nil {
-		return err
-	}
-
-	if err := tx.Commit(); err != nil {
-		return err
-	}
-
-	return nil
+	return db.RunInTx(ctx, nil, func(ctx context.Context, tx bun.Tx) error {
+		if _, err := tx.NewInsert().Model(book).Exec(ctx); err != nil {
+			return err
+		} else if err := insertBookRelation(ctx, tx, book.ID, book.Authors, newBookAuthor); err != nil {
+			return err
+		} else if err := insertBookRelation(ctx, tx, book.ID, book.Tags, newBookTag); err != nil {
+			return err
+		} else if err := insertBookRelation(ctx, tx, book.ID, book.Translators, newBookTranslator); err != nil {
+			return err
+		} else if err := insertBookRelation(ctx, tx, book.ID, book.Narrators, newBookNarrator); err != nil {
+			return err
+		}
+		return nil
+	})
 }
 
 func insertBookRelation[T any](ctx context.Context, tx bun.Tx, bookID int64, items []T, newLink func(bookID, id int64) any) error {
@@ -48,7 +39,6 @@ func insertBookRelation[T any](ctx context.Context, tx bun.Tx, bookID int64, ite
 			Model(&item).
 			Where("name = ?", name).
 			Scan(ctx)
-
 		if err != nil {
 			if _, err := tx.NewInsert().Model(&item).Exec(ctx); err != nil {
 				return fmt.Errorf("insert base '%s': %w", name, err)
@@ -67,39 +57,31 @@ func insertBookRelation[T any](ctx context.Context, tx bun.Tx, bookID int64, ite
 func UpdateBook(db *bun.DB, c echo.Context, id int64, book *model.Book) error {
 	ctx := c.Request().Context()
 
-	tx, err := db.BeginTx(ctx, nil)
-	if err != nil {
-		return fmt.Errorf("start tx: %w", err)
-	}
-	defer tx.Rollback()
+	return db.RunInTx(ctx, nil, func(ctx context.Context, tx bun.Tx) error {
+		book.ID = id
+		book.UpdatedAt = time.Now()
 
-	book.ID = id
-	book.UpdatedAt = time.Now()
+		_, err := tx.NewUpdate().
+			Model(book).
+			ExcludeColumn("created_at").
+			WherePK().
+			Exec(ctx)
+		if err != nil {
+			return fmt.Errorf("update book: %w", err)
+		}
 
-	_, err = tx.NewUpdate().
-		Model(book).
-		ExcludeColumn("created_at").
-		WherePK().
-		Exec(ctx)
-	if err != nil {
-		return fmt.Errorf("update book: %w", err)
-	}
+		if err := updateBookRelation(ctx, tx, book.ID, book.Authors, newBookAuthor, (*model.BookAuthor)(nil)); err != nil {
+			return fmt.Errorf("update relationships: %w", err)
+		} else if err := updateBookRelation(ctx, tx, book.ID, book.Tags, newBookTag, (*model.BookTag)(nil)); err != nil {
+			return fmt.Errorf("update relationships: %w", err)
+		} else if err := updateBookRelation(ctx, tx, book.ID, book.Translators, newBookTranslator, (*model.BookTranslator)(nil)); err != nil {
+			return fmt.Errorf("update relationships: %w", err)
+		} else if err := updateBookRelation(ctx, tx, book.ID, book.Narrators, newBookNarrator, (*model.BookNarrator)(nil)); err != nil {
+			return fmt.Errorf("update relationships: %w", err)
+		}
 
-	if err := updateBookRelation(ctx, tx, book.ID, book.Authors, newBookAuthor, (*model.BookAuthor)(nil)); err != nil {
-		return fmt.Errorf("update relationships: %w", err)
-	} else if err := updateBookRelation(ctx, tx, book.ID, book.Tags, newBookTag, (*model.BookTag)(nil)); err != nil {
-		return fmt.Errorf("update relationships: %w", err)
-	} else if err := updateBookRelation(ctx, tx, book.ID, book.Translators, newBookTranslator, (*model.BookTranslator)(nil)); err != nil {
-		return fmt.Errorf("update relationships: %w", err)
-	} else if err := updateBookRelation(ctx, tx, book.ID, book.Narrators, newBookNarrator, (*model.BookNarrator)(nil)); err != nil {
-		return fmt.Errorf("update relationships: %w", err)
-	}
-
-	if err := tx.Commit(); err != nil {
-		return fmt.Errorf("commit tx: %w", err)
-	}
-
-	return nil
+		return nil
+	})
 }
 
 func updateBookRelation[T any, L any](ctx context.Context, tx bun.Tx, bookID int64, items []T, newLink func(bookID, id int64) any, linkTable L) error {
@@ -113,8 +95,10 @@ func updateBookRelation[T any, L any](ctx context.Context, tx bun.Tx, bookID int
 
 	for _, item := range items {
 		name := reflect.ValueOf(item).FieldByName("Name").String()
-		if _, err := tx.NewInsert().Model(&item).Exec(ctx); err != nil {
-			return fmt.Errorf("insert base '%s': %w", name, err)
+		if err := tx.NewSelect().Model(&item).Where("name = ?", name).Scan(ctx); err != nil {
+			if _, err := tx.NewInsert().Model(&item).Exec(ctx); err != nil {
+				return fmt.Errorf("insert base '%s': %w", name, err)
+			}
 		}
 		id := reflect.ValueOf(&item).Elem().FieldByName("ID").Int()
 		link := newLink(bookID, id)
