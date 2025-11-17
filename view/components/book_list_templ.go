e6e09d66455ea69f990f2b8c8307b1d5a5ece389
Rentib <sbitner420@tutanota.com>
2025-11-17 16:37:02 +0100

    fix(book_list): make covers equal size

--- view/components/book_list_templ.go
+++ view/components/book_list_templ.go
@@ -171,7 +171,7 @@ func BookCover(book model.Book) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 11, "\" class=\"w-full aspect-[3/4]\"><img src=\"")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 11, "\" class=\"h-full\"><img src=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
