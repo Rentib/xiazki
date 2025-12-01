ea0d86ab94b17936ae9cbf9281ce345d172100db
Rentib <sbitner420@tutanota.com>
2025-12-01 19:10:51 +0100

    feat(auth): type the constants explicitly

--- web/template/auth/show_templ.go
+++ web/template/auth/show_templ.go
@@ -16,8 +16,8 @@ import (
 type Operation string
 
 const (
-	Login    = "login"
-	Register = "register"
+	Login    Operation = "login"
+	Register Operation = "register"
 )
 
 // TODO: create a struct with form values
@@ -175,7 +175,7 @@ func Form(data Data) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 8, "<div><button type=\"submit\" class=\"group relative flex w-full justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 8, "<div><button type=\"submit\" class=\"group relative flex w-full justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
